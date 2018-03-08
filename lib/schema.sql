c8602d7bdfd3e9012a2f9fb666a51d2cc490461b
Sean Oughton <seanoughton@gmail.com>
2018-03-08 18:37:31 +0000

    Done.

---
+++ lib/schema.sql
@@ -0,0 +1,38 @@
+CREATE TABLE series (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	title TEXT,
+  author_id INTEGER,
+  subgenre_id INTEGER
+);
+
+CREATE TABLE subgenres (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	name TEXT
+);
+
+CREATE TABLE authors (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	name TEXT
+);
+
+CREATE TABLE books (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	title TEXT,
+  year INTEGER,
+  series_id INTEGER
+);
+
+CREATE TABLE characters (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	name TEXT,
+  species TEXT,
+  motto TEXT,
+  series_id INTEGER,
+  author_id INTEGER
+);
+
+CREATE TABLE character_books (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	book_id INTEGER,
+	character_id INTEGER
+);
