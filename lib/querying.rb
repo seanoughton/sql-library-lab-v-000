65f8af35491ff54adcea77c619509c0b962e5800
Sean Oughton <seanoughton@gmail.com>
2018-03-08 17:13:14 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -27,7 +27,9 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "
-
+  SELECT authors.name, subgenres.name FROM authors
+  JOIN series ON series.author_id = authors.id
+  JOIN subgenres ON subgenres.id = series.subgenre_id 
   "
 end
 
